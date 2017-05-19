cd4e1c9e593faae8abbf271ab6e084ad561b6fb8
root <root@localhost.localdomain>
2017-05-18 23:19:48 -0500

    inicio del proyecto

---
+++ taller.c
@@ -0,0 +1,14 @@
+/* Inclusión de archivos */
+#include <stdio.h>
+#include <stdlib.h>
+/* Función principal */
+int main (int argc,char **argv)
+{
+   int llave = 0;
+   /* Impresión por pantalla y salida del programa*/
+   printf("cifrado ciclico\n");
+   printf("Ingrese la llave numerica:");
+   scanf ("%d", &llave);
+   printf("%i", llave);
+   return 0;
+}
